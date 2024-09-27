9a8cb7a7b978bd40c883308a6a358fdc91b84574
nomis51 <nomis51@users.noreply.github.com>
2024-09-26 20:25:26 -0400

    Cleanup

--- src/.old/iis/templates/index.ts
+++
@@ -1,9 +0,0 @@
-const TOKEN_TEMPLATE = "#{$0}";
-
-function replaceTemplateToken(template: string, token: string, value: string) {
-	return template.replaceAll(TOKEN_TEMPLATE.replace("$0", token), value);
-}
-
-export function replaceTemplateTokens(template: string, tokens: { [key: string]: string }) {
-	return Object.keys(tokens).reduce((acc, key) => replaceTemplateToken(acc, key, tokens[key]), template);
-}
